372e554ee8d2e1c2c35b3bd1cbfa41f4c866d1b8
kuldeep <kuldeep.solution900@gmail.com>
2022-10-09 18:49:30 +0530

    New Commit

--- index.js
+++ index.js
@@ -64,6 +64,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(multer().any());
 app.use(cors());
 // Connect to Database
 const db = require("./config/db");
